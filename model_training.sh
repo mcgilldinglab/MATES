9d0cbcfaf1a5c84989b2c8a7c753dedff8b19e28
Ruohan Wang <ruohan.wang4@mail.mcgill.ca>
2023-08-23 10:52:13 -0400

    refactor prediction for diff data mode

--- model_training.sh
+++ model_training.sh
@@ -10,7 +10,7 @@ helpFunction()
    echo -e "\t-t Threads number"
    echo -e "\t-f File contains sample name"
 #    echo -e "\t-p Path to STAR/STAR_Solo aligned bam folder"
-   echo -e "\t--data_mode 10X or Smart-seq"
+   echo -e "\t--data_mode 10X or Smart_seq"
    echo -e "\t--bin_size Bin size for identifying U/M region"
    echo -e "\t--proportion Proportion of dominating U/M reads in region"
    exit 1 # Exit script after printing help
@@ -49,25 +49,43 @@ then
    helpFunction
 fi
 
-
 bin_size = $((bin_size))
 proportion = $((proportion))
 threads_num = $((threads_num))
+##### Training the model #####
+# file_name = sys.argv[1]
+# BIN_SIZE = sys.argv[2]
+# PROP = sys.argv[3]
+BATCH_SIZE = 4096
+AE_LR = 1e-4
+MLP_LR = 1e-6
+EPOCHS = 200
+# data_mode = sys.argv[8]
 
-if [ "$input_command" = "10X" ]; then
-    ##### Training the model #####
-    python training_model.py $file_name $bin_size $proportion $data_mode
-    ##### Make prediction #####
-    python prediction.py $file_name $bin_size $proportion ./TE_nooverlap.csv  $data_mode
+python train_model.py $file_name $bin_size $proportion $batch_size $AE_LR $MLP_LR $EPOCHS $data_mode
 
-    ##### Sort out result matrix #####
+##### Make prediction #####
+python make_prediction.py $file_name $bin_size $proportion $batch_size ./TE_nooverlap.csv $data_mode
+
+
+##### Sort out result matrix #####
+if [ "$data_mode" = "Smart_seq" ]; then
     mkdir -p result_MTX
+    mv Combination/TE_MTX.csv result_MTX/TE_MTX.csv
+    mv Unique_TE/Unique_All_MTX.csv result_MTX/Unique_TE_MTX.csv
+    mv prediction/Multi_MTX.csv result_MTX/Multi_TE_MTX.csv
+    rm -r Combination
+    rm -r Unique_TE
+    rm -r prediction
+    done
 
+if [ "$data_mode" = "10X" ]; then
+    mkdir -p result_MTX
     for line in $(cat "$file_name"); do
         mkdir -p result_MTX/${line}
         mv Combination/${line}/TE_MTX.csv result_MTX/${line}/TE_MTX.csv
         mv Unique_TE/${line}/Unique_All_MTX.csv result_MTX/${line}/Unique_TE_MTX.csv
-        mv prediction/${line}_Multi.csv result_MTX/${line}/Multi_TE_MTX.csv
+        mv prediction/${line}/Multi_MTX.csv result_MTX/${line}/Multi_TE_MTX.csv
         rm -r Combination
         rm -r Unique_TE
         rm -r prediction
