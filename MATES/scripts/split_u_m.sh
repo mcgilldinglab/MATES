34d4298b64848bfd990472d8317164ca7584c510
Ruohan Wang <ruohan.wang4@mail.mcgill.ca>
2023-08-25 01:42:21 -0400

    Update split_u_m.sh

--- MATES/scripts/split_u_m.sh
+++ MATES/scripts/split_u_m.sh
@@ -1,8 +1,13 @@
 #!bin/bash
-cat $1 | while read line
-    do
-    samtools view -F 4 -bq 255 ./$2/${line}/${line}_Aligned.sortedByCoord.out.bam > ./unique_read/${line}_uniqueread.bam
-    samtools index ./unique_read/${line}_uniqueread.bam
-    samtools view -h ./$2/${line}/${line}_Aligned.sortedByCoord.out.bam | grep -vP "(NH:i:1\b)" | samtools view -Sb > ./multi_read/${line}_multireads.bam
-    samtools index ./multi_read/${line}_multireads.bam
-    done
+if [ -f "$1" ] && [ -f "$2" ]; then
+    for line in `cat "$1"`
+        do
+        samtools view "$2" | head
+        samtools view -F 4 -bq 255 "$2" > ./unique_read/${line}_uniqueread.bam
+        samtools index ./unique_read/${line}_uniqueread.bam
+        samtools view -h "$2" | grep -vP "(NH:i:1\b)" | samtools view -Sb > ./multi_read/${line}_multireads.bam
+        samtools index ./multi_read/${line}_multireads.bam
+        done
+else
+    echo "File '$1' or '$2' not found."
+fi
