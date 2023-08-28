70742c7c09a368aaec620e01a302f37536b0b10b
Ruohan Wang <ruohan.wang4@mail.mcgill.ca>
2023-08-27 20:20:28 -0400

    update pip

--- MATES/setup.py
+++ MATES/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="MATES",
-    version="0.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=["matplotlib==3.7.2",
         "numpy==1.25.2",
