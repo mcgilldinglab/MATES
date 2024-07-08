8e4f371e3f88de7fe04c0e8665fb021365c906cb
Ruohan Wang <ruohan.wang4@mail.mcgill.ca>
2024-07-08 23:24:34 +0800

    revert

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Ruohan Wang'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
+    'recommonmark',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
