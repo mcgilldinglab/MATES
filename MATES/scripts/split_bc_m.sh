0c5972cc57d87a1e1b18d2a721c4ecd103a6b28c
Yumin Zheng <37805910+Szym29@users.noreply.github.com>
2024-10-28 15:04:12 -0400

    Update split_bc_m.sh

--- MATES/scripts/split_bc_m.sh
+++ MATES/scripts/split_bc_m.sh
@@ -14,20 +14,27 @@ if [ -f "$1" ] && [ -f "$2" ]; then
         
         # Run the Python script
         python "$SCRIPT_DIR/split_bam_by_bc.py" "$3" ./multi_read/${line1}_multireads.bam \
-            ${line2} ./multi_read/${line1}/by_barcode/ >> ./multi_read/${line1}/${line1}_multi_splitting.log
+            ${line2} ./multi_read/${line1}/by_barcode/ "$4" >> ./multi_read/${line1}/${line1}_multi_splitting.log
         
         echo "End Processing ${line1}" >> ./multi_read/split_barcode.log
     done
 fi
 
 # Index the BAM files
+# for line in $(cat "$1"); do
+#     echo "Start Indexing Multi Bam in ${line}" >> ./multi_read/split_barcode.log
+    
+#     for file in ./multi_read/${line}/by_barcode/*.bam; do
+#         samtools sort ${file} -o ${file}
+#         samtools index ${file}
+#     done
+    
+#     echo "End Indexing Multi Bam in ${line}" >> ./multi_read/split_barcode.log
+# done
+
 for line in $(cat "$1"); do
     echo "Start Indexing Multi Bam in ${line}" >> ./multi_read/split_barcode.log
-    
-    for file in ./multi_read/${line}/by_barcode/*.bam; do
-        samtools sort ${file} -o ${file}
-        samtools index ${file}
-    done
-    
+    export line  # Export `line` so it's accessible to parallel subprocesses
+    find ./multi_read/${line}/by_barcode/*.bam | parallel -j "$4" --no-notice "samtools sort {} -o {} && samtools index {}"
     echo "End Indexing Multi Bam in ${line}" >> ./multi_read/split_barcode.log
 done
