13ce7346f53587fb2cb866d85d74211d08e90528
Yumin Zheng <37805910+Szym29@users.noreply.github.com>
2024-10-28 15:04:56 -0400

    Update split_bc_long.sh

--- MATES/scripts/split_bc_long.sh
+++ MATES/scripts/split_bc_long.sh
@@ -6,18 +6,23 @@ if [ -f "$1" ] && [ -f "$2" ]; then
     mkdir ./long_read/${line1}
     mkdir ./long_read/${line1}/by_barcode
     python MATES/scripts/split_bam_by_bc.py "$4" ${line2} ${line3} \
-     ./long_read/${line1}/by_barcode/ >> ./long_read/${line1}/${line1}_splitting.log
+     ./long_read/${line1}/by_barcode/ "$5" >> ./long_read/${line1}/${line1}_splitting.log
     echo "End Processing ${line1}" >> ./long_read/split_barcode.log
     done
 fi
-
-for line in `cat "$1"`
-    do
+for line in $(cat "$1"); do
     echo "Start Indexing Unique Bam in ${line}" >> ./long_read/split_barcode.log
-    for file in ./long_read/${line}/by_barcode/*.bam;
-        do
-        samtools sort ${file} -o ${file}
-        samtools index ${file}
-        done
+    export line  # Export `line` so it's accessible to parallel subprocesses
+    find ./long_read/${line}/by_barcode/*.bam | parallel -j "$4" --no-notice "samtools sort {} -o {} && samtools index {}"
     echo "End Indexing Unique Bam in ${line}" >> ./long_read/split_barcode.log
-    done
+done
+# for line in `cat "$1"`
+#     do
+#     echo "Start Indexing Unique Bam in ${line}" >> ./long_read/split_barcode.log
+#     for file in ./long_read/${line}/by_barcode/*.bam;
+#         do
+#         samtools sort ${file} -o ${file}
+#         samtools index ${file}
+#         done
+#     echo "End Indexing Unique Bam in ${line}" >> ./long_read/split_barcode.log
+#     done
