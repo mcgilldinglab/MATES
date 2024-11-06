3aaec02b4314a2de817aa162e235a1711fa354c6
Yumin Zheng <zhengyumin529@gmail.com>
2024-11-06 17:44:44 -0500

    v0.1.4 enhanced build_reference.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MATES",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     include_package_data=True,
     package_data={
