c400041a9e49548d195ae0e7017d3b1acde1b14b
Yumin Zheng <zhengyumin529@gmail.com>
2024-12-03 12:13:03 -0500

    update docs

--- MATES/MATES_model.py
+++ MATES/MATES_model.py
@@ -4,7 +4,7 @@ from MATES.scripts.make_prediction import make_prediction
 from MATES.scripts.make_prediction_locus import make_prediction_locus
 
 def train(data_mode, sample_list_file, bin_size = 5, proportion = 80, BATCH_SIZE= 4096, 
-          AE_LR = 1e-4, MLP_LR = 1e-6, AE_EPOCHS = 200, MLP_EPOCHS = 200, DEVICE= 'cude:0'):
+          AE_LR = 1e-4, MLP_LR = 1e-6, AE_EPOCHS = 200, MLP_EPOCHS = 200, DEVICE= 'cuda:0'):
     if data_mode != "10X" and data_mode != "Smart_seq":
         raise ValueError("Invalid data format. Supported formats are '10X' and 'Smart_seq'.")
 
@@ -20,7 +20,7 @@ def train(data_mode, sample_list_file, bin_size = 5, proportion = 80, BATCH_SIZE
         MATES_train(data_mode, sample_list_file, bin_size, proportion, BATCH_SIZE, AE_LR, MLP_LR, 
                  AE_EPOCHS, MLP_EPOCHS, DEVICE)
 
-def prediction(TE_mode, data_mode, sample_list_file, bin_size=5, proportion=80, AE_trained_epochs=200, MLP_trained_epochs=200, DEVICE= 'cude:0', ref_path = 'Default'):
+def prediction(TE_mode, data_mode, sample_list_file, bin_size=5, proportion=80, AE_trained_epochs=200, MLP_trained_epochs=200, DEVICE= 'cuda:0', ref_path = 'Default'):
     
     
     if data_mode != "10X" and data_mode != "Smart_seq":
@@ -46,7 +46,7 @@ def prediction(TE_mode, data_mode, sample_list_file, bin_size=5, proportion=80, 
     elif data_mode == 'Smart_seq':
         make_prediction(data_mode, bin_size, proportion, TE_ref_path, AE_trained_epochs, MLP_trained_epochs, None, DEVICE)
         
-def prediction_locus(TE_mode, data_mode, sample_list_file, bin_size=5, proportion=80, AE_trained_epochs=200, MLP_trained_epochs=200, DEVICE= 'cude:0', ref_path = 'Default'):
+def prediction_locus(TE_mode, data_mode, sample_list_file, bin_size=5, proportion=80, AE_trained_epochs=200, MLP_trained_epochs=200, DEVICE= 'cuda:0', ref_path = 'Default'):
     
     
     if data_mode != "10X" and data_mode != "Smart_seq":
